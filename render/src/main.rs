79b3b8002ae8bc32bdcda66555da8ee3a9995c78
abiggs33 <146982420+abiggs33@users.noreply.github.com>
2025-01-02 01:31:06 -0500

    sleep time

--- render/src/main.rs
+++ render/src/main.rs
@@ -13,13 +13,13 @@ use std::io::{stdout, Write};
 
 use math::Vec3;
 use buffer::Buffer;
-use viewmodel::ViewModel;
+use viewmodel::{ViewModel, draw3d};
 
 
 
 const HEIGHT: usize = 300;
 const WIDTH: usize = 400;
-const FPS: usize = 120;
+const FPS: usize = 100;
 
 type Float = f32;
 type Int = i32;
@@ -34,11 +34,28 @@ fn main() {
     window.set_target_fps(FPS);
     let mut buffer: Buffer = Buffer::cons(HEIGHT, WIDTH);
 
-    let mut viewmodel: ViewModel = ViewModel::cons(Vec3::cons(0, 0, 0), 0, 0);
+    let mut viewmodel: ViewModel = ViewModel::cons(Vec3::cons(0, 0, 0));
 
     while window.is_open() && !window.is_key_down(Key::Escape) {
         buffer.blackout();
         get_movement(&mut viewmodel, window.get_keys());
+        draw3d(&viewmodel, &mut buffer);
+
+        for x in 0..200 {
+            for y in 0..200 {
+                let color = (((x + y) as u32) << 16) | ((y as u32) << 8) | (x as u32);
+                buffer.set_pixel(x, y, color);
+            }
+        }
+        if (viewmodel.position.x as usize) < buffer.width()
+            && (viewmodel.position.y as usize) < buffer.height()
+        {
+            buffer.set_pixel(
+                viewmodel.position.x as usize,
+                viewmodel.position.y as usize,
+                0xFF00FFFF
+            );
+        }
 
         print!("\r\x1B[2K");
         print!("x: {}, y: {}, z: {}, rot: {}, tilt: {}",
@@ -58,8 +75,8 @@ fn get_movement(view: &mut ViewModel, keys: Vec<Key>)
 {
     keys.iter().for_each(|key| {
         match key {
-            Key::Q => view.rotate(1),
-            Key::E => view.rotate(-1),
+            Key::Q => view.rotate(1.0),
+            Key::E => view.rotate(-1.0),
             Key::W => view.translate(Vec3::cons(1, 0, 0)),
             Key::S => view.translate(Vec3::cons(-1, 0, 0)),
             Key::A => view.translate(Vec3::cons(0, -1, 0)),
--- render/src/viewmodel.rs
+++ render/src/viewmodel.rs
@@ -1,23 +1,28 @@
 
 
 
-use crate::Int;
+use std::f32::consts::PI;
+
+use crate::{Float, Int};
+use crate::buffer::Buffer;
 use crate::math::Vec3;
 
 
 
+const TAU: f32 = PI * 2.0;
+
 pub struct ViewModel
 {
     pub position: Vec3<Int>,
-    pub rotation: Int,
+    pub rotation: Float,
     pub tilt: Int,
 }
 
 impl ViewModel
 {
-    pub fn cons(position: Vec3<Int>, rotation: Int, tilt: Int) -> ViewModel
+    pub fn cons(position: Vec3<Int>) -> ViewModel
     {
-        ViewModel { position, rotation, tilt }
+        ViewModel { position, rotation: Float::default(), tilt: Int::default() }
     }
 
     pub fn translate(&mut self, direction: Vec3<Int>)
@@ -25,12 +30,20 @@ impl ViewModel
         self.position += direction;
     }
 
-    pub fn rotate(&mut self, delta: Int)
+    pub fn rotate(&mut self, delta: Float)
     {
         self.rotation += delta;
-        if self.rotation < 0 {
-            self.rotation += 360
+        if self.rotation < 0.0 {
+            self.rotation += TAU;
         }
+        else if self.rotation > TAU {
+            self.rotation -= TAU;
+        }
+    }
+
+    pub fn pos_float(&self) -> Vec3<Float>
+    {
+        Vec3::cons(self.position.x as Float, self.position.y as Float, self.position.z as Float)
     }
 
     pub fn tilt(&mut self, delta: Int)
@@ -39,3 +52,34 @@ impl ViewModel
         self.tilt = self.tilt.clamp(-70, 70);
     }
 }
+
+pub fn draw3d(view: &ViewModel, buffer: &mut Buffer)
+{
+    let (sine, cosine): (Float, Float) = view.rotation.sin_cos();
+    let position: Vec3<Float> = view.pos_float();
+    
+    // let x1 = 40 - view.position.x;
+    // let y1 = 10 - view.position.y;
+    // let z1 = 0.;
+
+    // let mut wx1: Float = (x1 as Float) * cosine - (y1 as Float) * sine;
+    // let mut wy1: Float = (y1 as Float) * cosine + (x1 as Float) * sine;
+
+    // wx1 = wx1 * 200. / wy1 + (buffer.width() as Float) / 2.;
+    // wy1 = z1 * 200. / wy1 + (buffer.height() as Float) / 2.;
+
+    // let drawx = wx1 as usize;
+    // let drawy = wy1 as usize;
+
+    // if drawx < buffer.width() && drawy < buffer.height() {
+    //     buffer.set_pixel(drawx, drawy, 0xFF00FFFF);
+    // }
+    //
+
+    let x = 40.0 - position.x;
+    let y = 10.0 - position.y;
+
+    let wx = x * cosine - y * sine;
+    let wy = y * cosine + x * sine;
+}
+
